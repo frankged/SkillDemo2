06323cb4a3f3f7abd69b24114b09fd40c4f4ee4a
frankged <frank.drugge@gmail.com>
2022-02-04 19:18:28 -0800

    Fail test + jars

--- SkillDemo2.java
+++ SkillDemo2.java
@@ -1,2 +1,5 @@
 public class SkillDemo2 {
+    public static int subtract3(int a) {
+        return a - 3;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo2Test {
+    @Test
+    public void subtract() {
+    assertEquals(1 , SkillDemo2.subtract3(5));
+    
+    }
+}
